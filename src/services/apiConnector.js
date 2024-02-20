368236e8b59f8fd2d20e335d61f47ce855ff82a7
HarshalMjn <mahajanh006@gmail.com>
2024-02-20 14:30:28 +0530

    Axios for Backend Call

---
+++ src/services/apiConnector.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+
+export const axiosInstance = axios.create({});
+
+export const apiConnector = (method, url, bodyData, hraders, parans) => {
+    return axiosInstance( {
+        method: `${method}`,
+        url: `${url}`,
+        data: bodyData ? bodyData : null,
+        headers:hraders ? hraders: null,
+        params: parans ? parans : null,
+
+    })
+}
