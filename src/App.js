83854f0005c34157232b4f415923886fe0120770
HarshalMjn <mahajanh006@gmail.com>
2024-02-24 22:40:26 +0530

    Login and Signup,send otp for email verification and connect front-end with back-end

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import "./App.css";
 import {Route, Routes } from "react-router-dom";
 import  Home  from "./Pages/Home"
 import Navbar from "./components/common/Navbar"
+import OpenRoute from "./components/core/auth/OpenRoute"
+
+import Login from "./Pages/Login"
+import Signup from './Pages/Signup'
+
 
 
 function App() {
@@ -10,6 +15,24 @@ function App() {
     <Navbar/>
     <Routes>
     <Route path="/" element={<Home/>} />
+    <Route
+        path="signup"
+        element={
+          <OpenRoute>
+            <Signup/>
+          </OpenRoute>
+        }>  
+      </Route>
+      <Route
+           path="login"
+           element={
+            <OpenRoute>
+              <Login/>
+            </OpenRoute>
+           }>
+        
+
+      </Route>
     </Routes>
    </div>
   );
